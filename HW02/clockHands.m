35788d60683414a3ccbfbd07bba6662df24a8c4b
trmontgomery <tiffany.re.montgomery@gmail.com>
2017-09-01 19:48:27 -0400

    Fixed clock hands

--- HW02/clockHands.m
+++ HW02/clockHands.m
@@ -2,9 +2,8 @@ function [updated_hour, updated_min] = clockHands(current_h, current_m, update)
     %Convert current time to minutes
     total_time = (current_h.*60) + current_m;
     %do the update
-    updated_time = 720 + total_time + update;
+    updated_time = total_time + update;
     %convert back to not-military time
-    updated_hour = floor(updated_time./60);
+    updated_hour = mod(floor(updated_time./60), 12);
     updated_min = mod(updated_time, 60);
-
 end
